5f8519ae15eb3257f7e29212ed64849e957ddbb1
Priyanka <Priyanka@LAPTOP-VNPRV150>
2020-08-17 14:39:16 +0530

    first commit

---
+++ Feature/src/test/java/package1/Name.java
@@ -0,0 +1,22 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Name {
+	@Test
+	public void test1()
+	{
+		System.out.println("i m first test");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("i m second test");
+	}
+	@Test
+	public void test3()
+	{
+		System.out.println("i m third test");
+	}
+
+}
